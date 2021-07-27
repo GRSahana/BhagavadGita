8c4cedeeb723e87fea15e76f90fed4049a7f1ab4
unknown <sahanabhatgr@gmail.com>
2021-07-27 17:54:00 +0530

    adding bottomNavigationBar, a chapterlist and favorite tab

--- app/src/main/java/com/nannaapp/bhagavadgita/MainActivity.kt
+++ app/src/main/java/com/nannaapp/bhagavadgita/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.nannaapp.bhagavadgita
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.nannaapp.bhagavadgita.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,20 +16,28 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
 
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
         navController = navHostFragment.findNavController()
         setSupportActionBar(findViewById(R.id.toolbar))
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigation,
+            navHostFragment!!.navController
+        )
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp()
     }
-
 
 }
---
+++ app/src/main/java/com/nannaapp/bhagavadgita/ui/fragment/FavoriteVerseFragment.kt
@@ -0,0 +1,7 @@
+package com.nannaapp.bhagavadgita.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.nannaapp.bhagavadgita.R
+
+class FavoriteVerseFragment: Fragment(R.layout.fragment_fav_verse) {
+}
